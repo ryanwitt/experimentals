c5bf08c7712a2a044f3a3442a607617faa01cb34
ryanwitt <onecreativenerd@gmail.com>
2010-05-09 20:24:25 -0700

    Fix a user reported bug that proton nmr shifts were out of order. Turns out this was due to the related object manager returning object in it's own random order. I suspect this may be a django bug. The workaround is to write a custom function on the object that makes the right call.

--- experimentals_generator/models.py
+++ experimentals_generator/models.py
@@ -1,84 +1,89 @@
 from django.db.models import *
 
 class Molecule(Model):
-	name = CharField(blank=True, max_length=255)
+    name = CharField(blank=True, max_length=255)
 
-	def __unicode__(self):
-		return u'%s' % self.name
+    def __unicode__(self):
+        return u'%s' % self.name
 
-	@permalink
-	def get_absolute_url(self):
-		return ('experimentals_generator.views.experimental', [str(self.id)])
+    @permalink
+    def get_absolute_url(self):
+        return ('experimentals_generator.views.experimental', [str(self.id)])
+
+    @property
+    def protonnmrshifts(self):
+        return self.protonnmrshift_set.order_by('id')
+
 
 class OpticalRotation(Model):
-	molecule = ForeignKey('Molecule')
-	temperature = CharField(blank=True, max_length=32)
-	concentration = CharField(blank=True, max_length=32)
-	solvent = CharField(blank=True, max_length=32)
-	value = CharField(blank=True, max_length=32)
-	class Meta:
-		verbose_name = 'Optical Rotation'
+    molecule = ForeignKey('Molecule')
+    temperature = CharField(blank=True, max_length=32)
+    concentration = CharField(blank=True, max_length=32)
+    solvent = CharField(blank=True, max_length=32)
+    value = CharField(blank=True, max_length=32)
+    class Meta:
+        verbose_name = 'Optical Rotation'
 
-class GCRetentionTime(Model):	
-	molecule = ForeignKey('Molecule')
-	major_retention_time = CharField(blank=True, max_length=32)
-	minor_retention_time = CharField(blank=True, max_length=32)
-	class Meta:
-		verbose_name = 'GC Retention Time'
+class GCRetentionTime(Model):    
+    molecule = ForeignKey('Molecule')
+    major_retention_time = CharField(blank=True, max_length=32)
+    minor_retention_time = CharField(blank=True, max_length=32)
+    class Meta:
+        verbose_name = 'GC Retention Time'
 
 class ProtonNMR(Model):
-	molecule = ForeignKey('Molecule')
-	frequency = CharField(blank=True, max_length=32)
-	solvent = CharField(blank=True, max_length=32)
-	class Meta:
-		verbose_name = 'Proton NMR'
+    molecule = ForeignKey('Molecule')
+    frequency = CharField(blank=True, max_length=32)
+    solvent = CharField(blank=True, max_length=32)
+    class Meta:
+        verbose_name = 'Proton NMR'
 
 class ProtonNMRShift(Model):
-	molecule = ForeignKey('Molecule')
-	shift = CharField(blank=True, max_length=32)
-	peak_type = CharField(blank=True, max_length=32)
-	integration = CharField(blank=True, max_length=32)
-	j_value_1 = CharField(blank=True, max_length=8)
-	j_value_2 = CharField(blank=True, max_length=8)
-	j_value_3 = CharField(blank=True, max_length=8)
-	j_value_4 = CharField(blank=True, max_length=8)
-	class Meta:
-		verbose_name = 'Proton NMR Shift'
+    molecule = ForeignKey('Molecule')
+    shift = CharField(blank=True, max_length=32)
+    peak_type = CharField(blank=True, max_length=32)
+    integration = CharField(blank=True, max_length=32)
+    j_value_1 = CharField(blank=True, max_length=8)
+    j_value_2 = CharField(blank=True, max_length=8)
+    j_value_3 = CharField(blank=True, max_length=8)
+    j_value_4 = CharField(blank=True, max_length=8)
+    class Meta:
+        verbose_name = 'Proton NMR Shift'
 
 class CarbonNMR(Model):
-	molecule = ForeignKey('Molecule')
-	frequency = CharField(blank=True, max_length=32)
-	solvent = CharField(blank=True, max_length=32)
-	class Meta:
-		verbose_name = 'Carbon NMR'
+    molecule = ForeignKey('Molecule')
+    frequency = CharField(blank=True, max_length=32)
+    solvent = CharField(blank=True, max_length=32)
+    class Meta:
+        verbose_name = 'Carbon NMR'
 
 class CarbonNMRShift(Model):
-	molecule = ForeignKey('Molecule')
-	shift = CharField(blank=True, max_length=8)
-	class Meta:
-		verbose_name = 'Carbon NMR Shift'
+    molecule = ForeignKey('Molecule')
+    shift = CharField(blank=True, max_length=8)
+    class Meta:
+        verbose_name = 'Carbon NMR Shift'
 
 class IRType(Model):
-	molecule = ForeignKey('Molecule')
-	type = CharField(blank=True, max_length=32)
-	class Meta:
-		verbose_name = 'IR Type'
+    molecule = ForeignKey('Molecule')
+    type = CharField(blank=True, max_length=32)
+    class Meta:
+        verbose_name = 'IR Type'
 
 class IRPeak(Model):
-	molecule = ForeignKey('Molecule')
-	value = CharField(blank=True, max_length=8)
-	class Meta:
-		verbose_name = 'IR Peak'
+    molecule = ForeignKey('Molecule')
+    value = CharField(blank=True, max_length=8)
+    class Meta:
+        verbose_name = 'IR Peak'
     
 class HRMS(Model):
-	molecule = ForeignKey('Molecule')
-	type = CharField(blank=True, max_length=32)
-	formula = CharField(blank=True, max_length=32)
-	fragment_type = CharField(blank=True, max_length=32)
-	fragment_charge = BooleanField(blank=True, help_text = 'check for positive')
-	found = CharField(blank=True, max_length=32)
-	calculated = CharField(blank=True, max_length=32)
-	class Meta:
-		verbose_name = 'High Res Mass Spec'
+    molecule = ForeignKey('Molecule')
+    type = CharField(blank=True, max_length=32)
+    formula = CharField(blank=True, max_length=32)
+    fragment_type = CharField(blank=True, max_length=32)
+    fragment_charge = BooleanField(blank=True, help_text = 'check for positive')
+    found = CharField(blank=True, max_length=32)
+    calculated = CharField(blank=True, max_length=32)
+    class Meta:
+        verbose_name = 'High Res Mass Spec'
 
 
